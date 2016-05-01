704d0b6de43976078e38e5669ce868cece53c128
ABeltramo <beltramo.ale@gmail.com>
2016-05-02 01:50:40 +0200

    Added OpenVPN docker

--- userdata.sh
+++ userdata.sh
@@ -9,6 +9,9 @@ sleep 5; # Wait for mount
 chown -R ubuntu /etc/letsencrypt/
 chmod -R ugo+rw /etc/letsencrypt/
 # Start docker containers
-/usr/bin/docker run --name CopiaIncollaDB -v /mnt/DATA/mysql/:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=[*INSERT ROOT PASSWORD HERE!*] -d -p 3306:3306 --restart=always mysql/mysql-server:5.5 --lower_case_table_names=1
+/usr/bin/docker run --name CopiaIncollaDB -v /mnt/DATA/mysql/:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=alepollo93! -d -p 3306:3306 --restart=always mysql/mysql-server:5.5 --lower_case_table_names=1
 cd /mnt/DATA/ci-nodedb-api/ && sh runme.sh
 cd /mnt/DATA/imap/ && /usr/local/bin/docker-compose up -d mail
+# OpenVPN
+sudo docker run --volumes-from openVPNData -d -p 1194:1194/udp --privileged --name openVPN kylemanna/openvpn
+sudo docker run --name openVPNData -v /mnt/DATA/ovpn-data/:/etc/openvpn busybox
