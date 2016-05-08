efcaa979010aad9bd446178fbf9e0c6a4f3f319f
ale <ale@Debian.localdomain>
2016-05-08 16:11:23 +0200

    Fixed errors

--- script.js
+++ script.js
@@ -45,7 +45,10 @@ function attachVolume(instanceId){
 					VolumeId: dataVolumeID,
 				}, function(err, data) {
 				if (err) console.log(err, err.stack); // an error occurred
-				else     console.log("DATA volume attached succesfully");
+				else {
+					console.log("DATA volume attached succesfully");
+					callback("Completed");
+				}
 			});
 	}); //End wait for volumeAvailable
 }
@@ -98,5 +101,4 @@ exports.handler = (event, context, callback) => {
     		});
     	}); // End wait for instanceRunning
     });
-    callback("Completed");
 };
