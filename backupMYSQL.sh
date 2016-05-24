bc21ac68cd619e1ed03a3e731968970b562f2e39
ABeltramo <beltramo.ale@gmail.com>
2016-05-25 00:48:10 +0200

    Removed unnecessary logs

--- backupMYSQL.sh
+++ backupMYSQL.sh
@@ -3,13 +3,13 @@
 DUMP=/tmp/mysqldump.sql
 /usr/bin/docker exec -i CopiaIncollaDB mysqldump --all-databases -ubackup > $DUMP
 DUMPTAR=/tmp/mysqldump.tgz
-/bin/tar -zcf $DUMPTAR $DUMP
+/bin/tar -zcf $DUMPTAR $DUMP > /dev/null
 /mnt/DATA/s3cmd/s3cmd-1.6.1/s3cmd --config /mnt/DATA/s3cmd/s3cfg put $DUMPTAR s3://bkp-srv-ec2/mysql/
 # Stop docker istance
 # /usr/bin/docker kill CopiaIncollaDB
 # Backup mysql folder
 BACKUPFILE=/tmp/mysqlFolder.tgz
-/bin/tar -zcvf $BACKUPFILE /mnt/DATA/mysql
+/bin/tar -zcvf $BACKUPFILE /mnt/DATA/mysql > /dev/null
 /mnt/DATA/s3cmd/s3cmd-1.6.1/s3cmd --config /mnt/DATA/s3cmd/s3cfg put $BACKUPFILE s3://bkp-srv-ec2/mysql/
 # Clean
 rm $BACKUPFILE
